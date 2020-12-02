908ceca2843e258b4979767f59fdabf695deeb64
Lex Torbanov <torbanowmain@gmail.com>
2020-12-02 15:20:26 -0800

    Fuck this bulling

---
+++ README.md
@@ -0,0 +1,1 @@
+Fuck this system! I had problems with virtual box on instaling Mac OS and running project. Fuck it! It's all.
---
+++ WhatTheFoxSay/WhatTheFoxSay/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatTheFoxSay
+//
+//  Created by Lex Torbanov on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
